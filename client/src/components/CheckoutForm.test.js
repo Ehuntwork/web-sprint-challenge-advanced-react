eed3fd49ccb705327c7d31781fc66982c52563b8
emilme <emilme@sir.mle>
2020-07-10 09:36:32 -0700

    Test two passing as expected

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -10,4 +10,12 @@ test("form header renders", () => {
     expect(header).toBeInTheDocument()
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    //render page
+    render(<CheckoutForm/>)
+    //click checkout
+    const checkoutButton = screen.getByText(/^checkout$/i)
+    fireEvent.click(checkoutButton)
+    // sucessMessage there?
+    const successMessage = screen.getAllByTestId('successMessage')
+});
